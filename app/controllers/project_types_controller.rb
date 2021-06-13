d7719b3da92fbfe8c1844f3ed88c50875b65dfce
matthiashaefeli <haefeli.matthias2gmail.com>
2021-06-12 19:04:58 -0500

    project_type crud

--- app/controllers/project_types_controller.rb
+++ app/controllers/project_types_controller.rb
@@ -1,4 +1,6 @@
 class ProjectTypesController < ApplicationController
+  before_action :set_project_type, only: [:show, :edit, :update, :destroy]
+
   def index
     @project_types = ProjectType.order(:name)
   end
@@ -8,7 +10,7 @@ class ProjectTypesController < ApplicationController
   end
 
   def create
-    @project_type = ProjectType.new(params[:project_type])
+    @project_type = ProjectType.new(project_type_params)
     if @project_type.save
       flash[:success] = "Object successfully created"
       redirect_to @project_type
@@ -19,18 +21,16 @@ class ProjectTypesController < ApplicationController
   end
 
   def update
-    @project_type = ProjectType.find(params[:id])
-      if @project_type.update_attributes(params[:project_type])
-        flash[:success] = "Object was successfully updated"
-        redirect_to @project_type
-      else
-        flash[:error] = "Something went wrong"
-        render 'edit'
-      end
+    if @project_type.update_attributes(params[:project_type])
+      flash[:success] = "Object was successfully updated"
+      redirect_to @project_type
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
+    end
   end
 
   def destroy
-    @project_type = ProjectType.find(params[:id])
     if @project_type.destroy
       flash[:success] = 'Object was successfully deleted.'
       redirect_to project_types_url
@@ -38,5 +38,15 @@ class ProjectTypesController < ApplicationController
       flash[:error] = 'Something went wrong'
       redirect_to project_types_url
     end
+  end
+
+  private
+
+  def project_type_params
+    params.require(:project_type).permit(:name)
+  end
+
+  def set_project_type
+    @project_type = ProjectType.find(params[:id])
   end
 end
