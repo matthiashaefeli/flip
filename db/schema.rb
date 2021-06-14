1de0521579811f3107df416da34b7233903821ad
matthiashaefeli <haefeli.matthias2gmail.com>
2021-06-14 17:05:53 -0500

    provider crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_171326) do
+ActiveRecord::Schema.define(version: 2021_06_14_213704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 2021_06_13_171326) do
   create_table "projects", force: :cascade do |t|
     t.integer "project_type_id"
     t.string "name"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "telephone1"
+    t.string "telephone2"
+    t.string "telephone3"
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
