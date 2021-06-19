3cef015c8afe06f151aab7cbc3b5d0682d5abf19
matthiashaefeli <haefeli.matthias2gmail.com>
2021-06-18 19:59:47 -0500

    contractors crud

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :realtors
   resources :lenders
   resources :materials
+  resources :contractors
   root 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_215108) do
+ActiveRecord::Schema.define(version: 2021_06_19_004451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 2021_06_17_215108) do
     t.integer "status"
     t.string "initial_condition"
     t.integer "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contractors", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "telephone1"
+    t.string "telephone2"
+    t.string "telephone3"
+    t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
